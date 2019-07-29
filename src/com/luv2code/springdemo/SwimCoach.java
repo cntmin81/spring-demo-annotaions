99bdecbb1b817f0f2f7bbe1af5430fdf741b9cee
youngminshin <youngminshin@192.168.10.9>
2019-07-29 21:02:23 +0900

    Injecting value from config properties file

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,17 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 	
-	FortuneService fortuneService;
+	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
 	
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
@@ -16,6 +25,14 @@ public class SwimCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 }
